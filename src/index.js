e93605318ceb4d9ce5e06caab2a3a225d28d6179
CATHERINE MENG <catherine@CATHERINEs-MBP.hitronhub.home>
2020-01-18 18:26:33 -0800

    revert

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./pages/websitePage";
+import Website from "./pages/websitePage";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Website />, rootElement);
