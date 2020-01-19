c41608faaa8ffe099dd52969c525ca0984afded2
CATHERINE MENG <catherine@CATHERINEs-MBP.hitronhub.home>
2020-01-18 18:50:24 -0800

    Updates

---
+++ precache-manifest.22ca74be2fb57655fc48290a86e30bce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f96edd717c56f36eeb4de5071496110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b59501917d0c249a9e4f",
+    "url": "/static/js/2.25c5551d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.25c5551d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26e422e6a0793245456e",
+    "url": "/static/js/main.e1a9c8c1.chunk.js"
+  },
+  {
+    "revision": "3218f9e22fa2fb35f7ab",
+    "url": "/static/js/runtime-main.e5b6fa5b.js"
+  }
+]);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Website from "./pages/websitePage";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Website />, rootElement);
