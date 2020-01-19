efbb421f9e996f084a7191fa89daa61b56a2a5c7
CATHERINE MENG <catherine@CATHERINEs-MBP.hitronhub.home>
2020-01-18 20:06:52 -0800

    updated uikit import

--- src/pages/websitePage.jsx
+++ src/pages/websitePage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import "node_modules/uikit/src/less/uikit.theme.less";
+require("../../node_modules/uikit/src/less/uikit.theme.less");
 
 /**s
 * A Website component
